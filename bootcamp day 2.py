2a5c8b702b8f1254a58d7f418bd5eb2c983f36ec
Swetha0122 <85818702+Swetha0122@users.noreply.github.com>
2021-06-14 12:23:04 +0530

    Update bootcamp day 2.py

--- bootcamp day 2.py
+++ bootcamp day 2.py
@@ -4,3 +4,54 @@ print("30 days 30 hours challenge")
 
 #assinging a vale to a string
 Hours=("thirty")
+print(Hours)
+
+#indexing using string
+Days="Thirty days"
+print(Days[5])
+
+#print the particular charecter from certain text
+Challege="I will win"
+print(Challenge[8:10])
+
+#print the length of character
+Challenge="I will win"
+print(len(Challenge))
+
+#convert string into lower character
+Challenge="I will win"
+print(Challenge.lower())
+
+#String Concatenation-Joining two strings
+a="30 Days"
+b="30 hours"
+c=a+b
+print(c)
+
+#Adding space during conatenation
+a="30 Days"
+b="30 hours"
+c=a+""+b
+print(c)
+
+#casefold()-usage
+text="Thrity days and Thirty hours"
+
+x=text.casefold()
+print(x)
+
+x=text.capitalize()
+print(x)
+
+x=text.find('e')
+print(x)
+ 
+x=texr.isalpha()
+print(x)
+
+x=text.isalnum()
+print(x)
+
+
+
+
