fa3e9a2fe8a984d861a31acfb9dc4285b3be33e5
Swetha0122 <85818702+Swetha0122@users.noreply.github.com>
2021-06-15 22:08:21 +0530

    Create boot camp day 4.py

---
+++ boot camp day 4.py
@@ -0,0 +1,16 @@
+a=b=c=100
+print("The value of a=b=c=",a)
+print("Dividing the value of a by 10",a/10)
+print("Multiply the value of b by 50",b*50)
+print("Add the value of c with 60",c+60)
+
+x='stars'
+print("string:",x)
+x1=x.replace('a','g')
+print('Replaced string:',x1)
+
+a=11
+b=10.5
+print("The float value of a is",float(a))
+print("The int value of b is",int(b))
+
