e135ba27a0f7bcb9f5c7d7603381444cb3229c72
Swetha0122 <85818702+Swetha0122@users.noreply.github.com>
2021-06-18 20:27:47 +0530

    Create bootcamp day 7.py

---
+++ bootcamp day 7.py
@@ -0,0 +1,19 @@
+def values(x,y):
+    w=a+b
+    x=a-b
+    y=a/b
+    z=a*b
+    print("Addition of two integer is",w)
+    print("Subtration of two integer is",x)
+    print("Division of two integer is",y)
+    print("Multiplication of two integer is",z)
+a=int(input("Enter the value of a"))
+b=int(input("Enter the value of b",))
+values(a,b)
+
+
+def covid(x):
+    print("Patient Name =",Patientname)
+    print("Body temperature = 98 degree")
+Patientname=input("Enter the Patient Name:")
+covid(Patientname)
